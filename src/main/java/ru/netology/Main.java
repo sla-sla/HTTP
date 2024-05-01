385a864f533da8c6d7f26d6c21d65f4630b28234
Вячеслав Бекиров <60131716@leroymerlin.ru>
2024-05-01 23:50:15 +0300

    First commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Server server = new Server();
+    server.startServer();
+  }
+}
+
